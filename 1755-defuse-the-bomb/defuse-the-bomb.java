3b1b8fc72cda68ec34cfdf331a779a9415ad1420
gurmeetofficial <166908658+gurmeetofficial@users.noreply.github.com>
2025-06-25 22:38:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (24.72%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -11,8 +11,6 @@ class Solution {
         int itr=absK;
         for(int i=0;i<len;i++) {
             if(itr>=len) itr=0;
-            System.out.println(i+" - "+itr+" - "+sum);
-            System.out.println(code[i]+" - "+code[itr]);
             if(k>0) {
                 sum = sum - code[i]+code[itr];
                 res[i]=sum;
