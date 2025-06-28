0d19c5f40a46f9258ef54b5b56c74b075d1a1e84
gurmeetofficial <166908658+gurmeetofficial@users.noreply.github.com>
2025-06-28 20:17:34 +0530

    Time: 41 ms (54.88%) | Memory: 44.9 MB (11.79%) - LeetSync

--- 897-prime-palindrome/prime-palindrome.java
+++ 897-prime-palindrome/prime-palindrome.java
@@ -1,8 +1,10 @@
 class Solution {
-    public Boolean checkPrime(int n) {
-        if(n==1) return false;
-        for(int i=2;i<=n/2;i++) {
-            if(n%i==0) return false;
+    private boolean checkPrime(int x) {
+        if (x < 2) return false;
+        if (x == 2) return true;
+        if (x % 2 == 0) return false;
+        for (int i = 3; i * i <= x; i += 2) {
+            if (x % i == 0) return false;
         }
         return true;
     }
