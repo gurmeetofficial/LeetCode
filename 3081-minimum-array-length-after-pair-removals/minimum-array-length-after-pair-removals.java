db281154833ec9b2c2312cf45c182081ae97daf1
gurmeetofficial <166908658+gurmeetofficial@users.noreply.github.com>
2025-06-29 21:59:59 +0530

    Time: 84 ms (5.08%) | Memory: 61.5 MB (18.64%) - LeetSync

--- 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
+++ 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
@@ -1,16 +1,15 @@
 class Solution {
     public int minLengthAfterRemovals(List<Integer> nums) {
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        int len = nums.size();
+        int len = nums.size(), minKey, minKeyVal;
         int res = len;
         for (int i = 0; i < len; i++) {
             if (i < len / 2) {
                 map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) + 1);
             } else {
                 if (map.size() > 0) {
-                    int minKey = map.firstKey();
-                    int minKeyVal = map.get(minKey);
-                    System.out.println(minKey);
+                    minKey = map.firstKey();
+                    minKeyVal = map.get(minKey);
                     if (minKey < nums.get(i) && minKeyVal > 0) {
                         res -= 2;
                         if (minKeyVal == 1) {
