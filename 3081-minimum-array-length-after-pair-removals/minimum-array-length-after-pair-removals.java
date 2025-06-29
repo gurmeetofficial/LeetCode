1d72eb07daff9ead77f216da0791ac3a4957d95b
gurmeetofficial <166908658+gurmeetofficial@users.noreply.github.com>
2025-06-29 22:03:54 +0530

    Time: 5 ms (97.18%) | Memory: 59.6 MB (51.98%) - LeetSync

--- 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
+++ 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
@@ -1,30 +1,16 @@
 class Solution {
     public int minLengthAfterRemovals(List<Integer> nums) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        int len = nums.size(), minKey, minKeyVal;
-        int res = len;
-        for (int i = 0; i < len; i++) {
-            if (i < len / 2) {
-                map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) + 1);
+        int i=0,n=nums.size(),j=n/2;
+        int res=0;
+        while(i<n/2 && j<n) {
+            if(nums.get(i)<nums.get(j)) {
+                i++;
+                j++;
+                res++;
             } else {
-                if (map.size() > 0) {
-                    minKey = map.firstKey();
-                    minKeyVal = map.get(minKey);
-                    if (minKey < nums.get(i) && minKeyVal > 0) {
-                        res -= 2;
-                        if (minKeyVal == 1) {
-                            map.remove(minKey);
-                        } else {
-                            map.put(minKey, map.get(minKey) - 1);
-                        }
-                    } else {
-                        map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) + 1);
-                    }
-                } else {
-                    map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) + 1);
-                }
+                j++;
             }
         }
-        return res;
+        return n-(res*2);
     }
 }
