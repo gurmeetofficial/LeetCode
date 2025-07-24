8434f559d26ab2bea60d7bfbb4182b5fe7c55093
gurmeetofficial <166908658+gurmeetofficial@users.noreply.github.com>
2025-07-24 21:37:52 +0530

    Time: 1 ms (93.59%) | Memory: 43 MB (83.33%) - LeetSync

--- 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.java
+++ 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.java
@@ -1,25 +1,9 @@
 class Solution {
-    HashMap<String, Boolean> dp = new HashMap();
-    public boolean canSplitArrayRec(List<Integer> nums, int m, int left, int right, int sum) {
-        String mapKey = left + "," + right;
-        if(dp.containsKey(mapKey)) return dp.get(mapKey);
-        if(right-left<1) return true;
-        if(sum<m) return false;
-        Boolean res = (
-            canSplitArrayRec(nums, m, left+1, right, sum-nums.get(left))
-            ||
-            canSplitArrayRec(nums, m, left, right-1, sum-nums.get(right))
-        );
-        dp.put(mapKey, res);
-        return res;
-    }
-
     public boolean canSplitArray(List<Integer> nums, int m) {
-        int sum=0, len=nums.size();
-        if(len<3) return true;
-        for(int i=0;i<len;i++) {
-            sum+=nums.get(i);
+        if(nums.size()<3) return true;
+        for(int i=1;i<nums.size();i++) {
+            if(nums.get(i)+nums.get(i-1)>=m) return true;
         }
-        return canSplitArrayRec(nums, m, 0, len-1, sum);
+        return false;
     }
 }
