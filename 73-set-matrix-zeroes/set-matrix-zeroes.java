fdcbe6853c49bfcf833473462d91fa74bed43fe3
Gurmeet Singh <166908658+gurmeetofficial@users.noreply.github.com>
2025-08-11 22:36:23 +0530

    Time: 1 ms (72.39%) | Memory: 45.6 MB (84.30%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,44 +1,52 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-    boolean firstRowZero = false, firstColZero = false;
-    // Check if first row needs to be zeroed
-    for (int j = 0; j < matrix[0].length; j++) {
-        if (matrix[0][j] == 0) { firstRowZero = true; break; }
-    }
-    // Check if first column needs to be zeroed
-    for (int i = 0; i < matrix.length; i++) {
-        if (matrix[i][0] == 0) { firstColZero = true; break; }
-    }
-    // Use first row & column as markers
-    for (int i = 1; i < matrix.length; i++) {
-        for (int j = 1; j < matrix[0].length; j++) {
-            if (matrix[i][j] == 0) {
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
+        int rows = matrix.length,cols = matrix[0].length;
+        boolean firstColZero=false,firstRowZero=false;
+        for(int i=0;i<cols;i++) {
+            if(matrix[0][i]==0) {
+                firstRowZero=true;
+                break;
+            }
+        }
+        for(int i=0;i<rows;i++) {
+            if(matrix[i][0]==0) {
+                firstColZero=true;
+                break;
+            }
+        }
+        for(int row=1;row<rows;row++) {
+            for(int col=1;col<cols;col++) {
+                if(matrix[row][col]==0) {
+                    matrix[row][0]=0;
+                    matrix[0][col]=0;
+                }
+            }
+        }
+        for(int i=1;i<rows;i++) {
+            if(matrix[i][0]==0) {
+                for(int j=0;j<cols;j++) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        for(int j=1;j<cols;j++) {
+            if(matrix[0][j]==0) {
+                for(int i=0;i<rows;i++) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(firstColZero) {
+            for(int i=0;i<rows;i++) {
+                matrix[i][0]=0;
+            }
+        }
+
+        if(firstRowZero) {
+            for(int i=0;i<cols;i++) {
+                matrix[0][i]=0;
             }
         }
     }
-    // Zero columns based on first row markers
-    for (int j = 1; j < matrix[0].length; j++) {
-        if (matrix[0][j] == 0) {
-            for (int i = 1; i < matrix.length; i++)
-                matrix[i][j] = 0;
-        }
-    }
-    // Zero rows based on first column markers
-    for (int i = 1; i < matrix.length; i++) {
-        if (matrix[i][0] == 0) {
-            for (int j = 1; j < matrix[0].length; j++)
-                matrix[i][j] = 0;
-        }
-    }
-    // Zero first row and first column if needed
-    if (firstRowZero) {
-        for (int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
-    }
-    if (firstColZero) {
-        for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
-    }
-}
-
 }
