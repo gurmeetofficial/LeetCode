347313e2988172482c88ab230c81309aff8bf08a
Gurmeet Singh <166908658+gurmeetofficial@users.noreply.github.com>
2025-08-23 18:42:24 +0530

    Time: 13 ms (93.70%) | Memory: 44.9 MB (77.85%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -3,7 +3,7 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         int resCount=0, curCount=0, res=-1;
         for(int i=0;i<nums.length;i++) {
-            if(nums[i]%2==0) {
+            if((nums[i] & 1)==0) {
                 curCount = map.getOrDefault(nums[i],0)+1;
                 if(curCount>resCount) {
                     resCount = curCount;
